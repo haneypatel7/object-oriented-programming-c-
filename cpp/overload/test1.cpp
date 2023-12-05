689554d074d788a7438ff888cd876839f9137c50
haneypatel <haneypatel7@gmail.com>
2023-12-05 22:12:37 +0530

    update

--- cpp/overload/test1.cpp
+++ cpp/overload/test1.cpp
@@ -33,14 +33,26 @@ class student
         cout<<marks2;
     }
 
+    student operator-(student s);
+
     };
 
+student student :: operator-(student s)
+{
+        student temp;
+        temp.marks1=marks1-s.marks1;
+        temp.marks2=marks2-s.marks2;
+        return temp;
+
+}
     int main()
     {
-        student s1(10,20),s2(10,20);
+        student s1(12,22),s2(10,20);
         
         student s3=s1+s2;
+        student s4=s1-s2;
         s3.display();
+        s4.display();
         
         
     }
