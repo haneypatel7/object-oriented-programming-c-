655a89e63fbc23d94aa51c3a8388cc24c3056965
haneypatel <haneypatel7@gmail.com>
2023-12-05 22:04:07 +0530

    testing

---
+++ cpp/overload/test1.cpp
@@ -0,0 +1,46 @@
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+
+class student
+
+    {
+
+        int marks1,marks2;
+        public:
+        student()
+        {
+            marks1=0;
+            marks2=0;
+        }
+
+        student(int a,int b)
+        {
+            marks1=a;
+            marks2=b;
+        }
+    student operator+(student s)
+    {
+        student temp;
+        temp.marks1=marks1+s.marks1;
+        temp.marks2=marks2+s.marks2;
+        return temp;
+    }
+
+    void display()
+    {
+        cout<<marks1;
+        cout<<marks2;
+    }
+
+    };
+
+    int main()
+    {
+        student s1(10,20),s2(10,20);
+        
+        student s3=s1+s2;
+        s3.display();
+        
+        
+    }
