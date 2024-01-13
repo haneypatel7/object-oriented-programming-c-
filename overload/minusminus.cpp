73f7710fc5301d888fa5750fc6a68a03caeaf18d
haneypatel <haneypatel7@gmail.com>
2024-01-13 18:16:05 +0530

    --overload

---
+++ overload/minusminus.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+//using ++ overload overload the object propertie ++
+
+
+class demo
+{
+    int a;
+    public:
+    demo()
+    {
+        a=0;
+    }
+    demo(int x)
+    {
+        a=x;
+    }
+
+
+    void operator --()
+    {
+        a=a-1;
+    }
+
+    void getdata()
+    {
+        cout<<"after the overload object value"<<a<<endl;
+    }
+
+
+};
+
+int main()
+{
+    demo a;
+    --a;
+    a.getdata();
+
+
+
+    return 0;
+}
