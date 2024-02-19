a5ddb6ae59e4a2b71de84508b082821773c5fc29
haneypatel <haneypatel7@gmail.com>
2024-02-19 23:01:50 +0530

    ll

---
+++ linkedlist/doubly.cpp
@@ -0,0 +1,83 @@
+#include<iostream>
+using namespace std;
+
+class dll
+{
+    class node
+    {
+        public:
+        int data;
+        node* next;
+        node* prev;
+        public:
+        node(int data)
+        {
+            this->data=data;
+            this->next=NULL;
+            this->prev=NULL;
+        }
+
+    };
+    node * head=NULL;
+    public:
+
+    void insertfirst(int data)
+    {
+        node* newnode=new node(data);
+        if(head==NULL)
+        {
+            head=newnode;
+            cout<<"data inserted successfully"<<endl;
+        }
+        else
+        {
+            newnode->next=head;
+            head=newnode;
+            cout<<"data inserted"<<endl;
+        }
+    }
+    public:
+    void display()
+    {
+        node *temp=head;
+        while(temp!=NULL)
+        {
+            cout<<temp->data<<endl;
+            temp=temp->next;
+        }
+    }
+
+    void insertLast(int data)
+    {
+            
+         node *temp=head;
+         node *newnode=new node(data);
+         if(head==NULL)
+        {
+            head=newnode;
+            cout<<"data inserted successfully"<<endl;
+        }
+        else
+        {
+            while(temp->next!=NULL)
+            {
+                temp=temp->next;
+            }
+            temp->next=newnode;
+            newnode->prev=newnode;
+            cout<<"inserted data at last sucessfully"<<endl;
+        }
+
+    }
+};
+
+int main()
+{
+    dll d;
+    d.insertfirst(1);
+    d.insertfirst(2);
+    d.insertfirst(3);
+    d.insertLast(4);
+    d.display();
+    return 0;
+}
